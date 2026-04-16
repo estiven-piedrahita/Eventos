0cbdba23bf7d81bf48e4fcbacbd16e27be05599d
estiven-piedrahita <duvanestivenpiedrahita@gmail.com>
2026-04-16 18:59:59 -0500

    Initial commit

---
+++ Controllers/PanelAdministrativoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eventos.Controllers;
+
+public class PanelAdministrativoController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    public  IActionResult Show()
+    {
+        return View();
+    }
+    public  IActionResult Create()
+    {
+        return View();
+    }
+    public  IActionResult Edit()
+    {
+        return View();
+    }
+    public  IActionResult Delete()
+    {
+        return View();
+    }
+    
+    
+    
+}
---
+++ Models/Evento.cs
@@ -0,0 +1,11 @@
+﻿namespace Eventos.Models;
+
+public class Evento
+{
+    int Id { get; set; }
+    string Nombre { get; set; }
+    DateTime Fecha  { get; set; } =  DateTime.Now;
+    string Descripcion { get; set; }
+    string Ubicacion { get; set; }
+    string Imagen { get; set; }
+}
